c88495beb9558e73096e12f2dbb42475dfa0adfc
José Fernando <jf8065572@gmail.com>
2024-04-03 17:53:52 -0300

    Back-end adicionado

---
+++ server.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+
+//Configurações de onde o servidor vai se localizar
+const hostname = 'localhost';
+const PORT = process.env.PORT || 3000;
+
+//Deixando estático
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Pegando rota
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, hostname, () => {
+    console.log(`Servidor rodando em http://${hostname}:${PORT}`);
+});
